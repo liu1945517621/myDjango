ca075fc957d499e014383744a1e3db922ac01a4b
liu1945517621@qq.com <1945517621@qq.com>
2025-07-19 19:05:00 +0800

    新建项目

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -31,15 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user.apps.UserConfig',
+    'role.apps.RoleConfig',
+    'menu.apps.MenuConfig',
 
     # 我的应用
-    'my_testapp01',
+    # 'user',
+    # 'role',
+    # 'menu',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +82,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.myaql',
+        'NAME': 'db_admin3',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3308',
     }
 }
 
